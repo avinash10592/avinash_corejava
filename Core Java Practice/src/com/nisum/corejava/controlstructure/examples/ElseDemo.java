609831a0ac417587c11004626c8ae6af9cc74504
Nisum <Nisum@HYD-LAP-00159.corp.nisum.com>
2020-01-07 20:18:13 +0530

    Core Java POC's

--- Core Java Practice/src/com/nisum/corejava/controlstructure/examples/ElseDemo.java
+++ Core Java Practice/src/com/nisum/corejava/controlstructure/examples/ElseDemo.java
@@ -3,8 +3,8 @@ package com.nisum.corejava.controlstructure.examples;
 public class ElseDemo {
 
 	public static void main(String[] args) {
-		int a = 30;
-		if (a < 20) {
+		int num = 30;
+		if (num < 20) {
 			System.out.println("this is if ststement");
 		} else {
 			System.out.println("this is else ststement");
--- Core Java Practice/src/com/nisum/corejava/controlstructure/examples/Nestedif.java
+++ Core Java Practice/src/com/nisum/corejava/controlstructure/examples/Nestedif.java
@@ -2,11 +2,11 @@ package com.nisum.corejava.controlstructure.examples;
 
 public class Nestedif {
 	public static void main(String[] args) {
-		int a = 30;
-		int b = 40;
+		int num1 = 30;
+		int num2 = 40;
 
-		if (a == 30) {
-			if (b == 40) {
+		if (num1 == 30) {
+			if (num2 == 40) {
 				System.out.println("a =30 and y=40");
 			}
 		}
