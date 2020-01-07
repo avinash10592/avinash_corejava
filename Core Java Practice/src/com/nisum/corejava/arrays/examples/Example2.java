5d19f0a3c812a723fd79124f107a59fb5806a907
Nisum <Nisum@HYD-LAP-00159.corp.nisum.com>
2020-01-07 19:56:13 +0530

    Core Java POC's

--- Core Java Practice/src/com/nisum/corejava/arrays/examples/Example2.java
+++ Core Java Practice/src/com/nisum/corejava/arrays/examples/Example2.java
@@ -4,10 +4,10 @@ public class Example2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a[] = { 33, 3, 4, 5 };// declaration, instantiation and initialization
+		int arr[] = { 33, 3, 4, 5 };// declaration, instantiation and initialization
 		// printing array
-		for (int i = 0; i < a.length; i++)// length is the property of array
-			System.out.println(a[i]);
+		for (int in = 0; in < arr.length; in++)// length is the property of array
+			System.out.println(arr[in]);
 
 	}
 
