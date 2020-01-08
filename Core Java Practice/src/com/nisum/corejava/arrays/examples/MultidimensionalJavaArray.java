446639f66364acf312f4a257ac7f82e14d61407e
Nisum <Nisum@HYD-LAP-00159.corp.nisum.com>
2020-01-08 12:34:04 +0530

    Core Java POC's

--- Core Java Practice/src/com/nisum/corejava/arrays/examples/MultidimensionalJavaArray.java
+++ Core Java Practice/src/com/nisum/corejava/arrays/examples/MultidimensionalJavaArray.java
@@ -4,12 +4,11 @@ public class MultidimensionalJavaArray {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int arr[][] = { { 1, 2, 3 }, { 2, 4, 5 }, { 4, 4, 5 } };
+		double arr[][] = { { 1.2, 2.3, 3.4 }, { 2.4, 4.3, 5.2 }, { 4.2, 4.2, 5.2 } };
 		for (int in1 = 0; in1 < 3; in1++) {
 			for (int in2 = 0; in2 < 3; in2++) {
 				System.out.print(arr[in1][in2] + " ");
 			}
-			System.out.println();
 
 		}
 	}
