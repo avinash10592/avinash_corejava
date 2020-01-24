072bb259246ee09de65a4d8223fe6f79ab942317
avinash10592 <avi.nash10592@gmail.com>
2020-01-24 15:53:33 +0530

    corejava POC's

--- src/com/nisum/corejava/customclassaskeyinmap/example/Student.java
+++ src/com/nisum/corejava/customclassaskeyinmap/example/Student.java
@@ -3,14 +3,20 @@ package com.nisum.corejava.customclassaskeyinmap.example;
 public class Student {
     private int stu_rollNumber;
     private String stu_name;
+    private String stu_branch;
 
-    public Student(int rollNumber, String name) {
+    public Student(int rollNumber, String name,String branch) {
         this.stu_rollNumber = rollNumber;
         this.stu_name = name;
+        this.stu_branch = branch;
     }
 
     public int getRollNumber() {
         return stu_rollNumber;
+    }
+
+    public String getStu_branch() {
+        return stu_branch;
     }
 
     public void setRollNumber(int rollNumber) {
@@ -21,10 +27,14 @@ public class Student {
         return stu_name;
     }
 
+    public void setStu_section(String stu_branch) {
+        this.stu_branch = stu_branch;
+    }
+
     public void setName(String name) {
         this.stu_name = name;
     }
     public void display(){
-        System.out.println("Rollnumber: "+getRollNumber()+" Name: "+getName());
+        System.out.println("Rollnumber: "+getRollNumber()+" Name: "+getName()+" Branch: " + getStu_branch());
     }
 }
