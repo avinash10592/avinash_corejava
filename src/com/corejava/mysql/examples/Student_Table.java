03075e452b615c6c3eab80b129b23d404e66620b
avinash10592 <avi.nash10592@gmail.com>
2020-02-04 14:08:41 +0530

    corejava POC's

--- src/com/corejava/mysql/examples/Student_Table.java
+++ src/com/corejava/mysql/examples/Student_Table.java
@@ -1,7 +1,7 @@
 package com.corejava.mysql.examples;
 
 import com.corejava.util.MyLogger;
-
+//Step 1: importing the package (java.sql.*)
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -9,15 +9,26 @@ import java.sql.Statement;
 
 public class Student_Table {
     public static void main(String[] args) throws Exception {
+
         String url = "jdbc:mysql://localhost:3306/database1";
         String uname = "root";
         String password = "Lakshmi@2";
         String query = "SELECT * FROM database1.employee_table where emp_id=103";
+
+        //Step 2: loading and registering the driver
+        //registering the driver with forName method
         Class.forName("com.mysql.cj.jdbc.Driver");
+
+        //Step 3: establishing the connection with interface "Connection"
         Connection con = DriverManager.getConnection(url, uname, password);
+
+        //Step 4: creating the statement
         Statement st = con.createStatement();
+
+       //Step 5: execute the query
         ResultSet rs = st.executeQuery(query);
 
+        //Step 6:processing the results
         rs.next();
         String name = rs.getString("first name");
         int age = rs.getInt("age");
@@ -25,5 +36,10 @@ public class Student_Table {
         MyLogger.consoleLogger.info(name);
         MyLogger.consoleLogger.info(age);
         MyLogger.consoleLogger.info(department);
+
+        //Step 7: close
+        st.close();
+        con.close();
+
     }
 }
